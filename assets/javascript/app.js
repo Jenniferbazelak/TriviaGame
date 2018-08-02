71928a9688d167f272faa6e3271fb21652c8b99c
Baz <jennigoldstein@gmail.com>
2018-08-01 21:52:27 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 
-var count = 0;
+var index = 0;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var userGuess = 0;
@@ -12,52 +12,63 @@ $(document).ready(function () {
         {
             question: "What is the name of the cannon shot every time the Longhorns score a touchdown?",
             choices: ["A: Bevo the Cannon", "B: Bertha the Cannon", "C: Smokey The Cannon", "D: The Cannon"],
-            answer: "C"
+            answer: "C",
+            picture:"assets/images/cannon.jpg"
         },
         {
             question: "Which of the following is not a UT alumni?",
             choices: ["A: Matthew MCConaughey", "B: Rick Perry", "C: Kevin Durant", "D: Wes Anderson"],
-            answer: "B"
+            answer: "B",
+            picture:"assets/images/Matthew-McC.jpg"
+            
         },
         {
             question: "What is the name of the school mascot?",
             choices: ["A: Bevo", "B: Bertha", "C: Mr.Longhorn", "D: Buddy"],
-            answer: "A"
+            answer: "A",
+            picture:"assets/images/bevo.jpg"
         },
         {
-            question: "What is the name of the schools song?",
+            question: "What is the name of the school song?",
             choices: ["A: Texas,Texas", "B: Gabriel", "C: The Eyes of Texas", "D: Longhorn Pride"],
-            answer: "C"
+            answer: "C",
+            picture:"assets/images/eyesoftexas.jpg"
         },
         {
             question: "What are the school colors?",
             choices: ["A: Orange and White", "B: Maroon and White", "C: Red and Black", "D: Burnt-Orange and White"],
-            answer: "D"
+            answer: "D",
+            picture:"assets/images/colors.jpg"
         },
         {
             question: "How many acres of land was The University of Texas founded on?",
             choices: ["A: 200 Acres", "B: 30 Acres", "C: 40 Acres", "D: 100 Acres"],
-            answer: "C"
+            answer: "C",
+            picture:"assets/images/fortyacres.gif"
         },
         {
             question: "At a football game, what is the proper answer to 'Texas'?",
             choices: ["A: Go,Go,Go", "B: Fight", "C: Win", "D: Beat'em"],
-            answer: "B"
+            answer: "B",
+            picture:"assets/images/texasfight.jpeg"
         },
         {
-            question: "What is the name of the stadium where the longhorns play football?",
+            question: "What is the name of the stadium where the Longhorns play football?",
             choices: ["A: Longhorn Stadium", "B: Littlefield Memorial Stadium", "C: Mack's House", "D: Darrell K. Royal Texas Memorial Stadium"],
-            answer: "D"
+            answer: "D",
+            picture:"assets/images/stadium.jpeg"
         },
         {
             question: "The longhorns were Division 1-A National Champions in all of the following years except:",
             choices: ["A: 1963", "B: 1969", "C: 1970", "D: 2006"],
-            answer: "D"
+            answer: "D",
+            picture:"assets/images/champions.jpg"
         },
         {
             question: "What is the name of the famous longhorn hand-sign?",
             choices: ["A: High-horns", "B: Points-up", "C: Hook'em Horns", "D: Bevo"],
-            answer: "C"
+            answer: "C",
+            picture:"assets/images/hook.jpg"
 
 
         }];
@@ -72,31 +83,34 @@ $(document).ready(function () {
         $(".button").show();
         $("#question").show();
         $("#time").show();
-        $("#counter").text(counter);
+        $("#counter").text(counter + "seconds");
         $("#counter").show();
+        $(".card").show();
+        $(".card-img-top").attr("src",trivia[index].picture)
         timer();
     }
 
     function displayQuestion() {
-        $("#question").text(trivia[count].question)
+        $("#question").text(trivia[index].question)
     };
 
     function displayAnswers() {
-        $("#A").text(trivia[count].choices[0]);
-        $("#B").text(trivia[count].choices[1]);
-        $("#C").text(trivia[count].choices[2]);
-        $("#D").text(trivia[count].choices[3]);
+        $("#A").text(trivia[index].choices[0]);
+        $("#B").text(trivia[index].choices[1]);
+        $("#C").text(trivia[index].choices[2]);
+        $("#D").text(trivia[index].choices[3]);
     };
 
     function timer() {
         clock= setInterval(countdown, 2000);
         function countdown() {
-            if (counter >1){
+            if (counter >0){
                 counter--;
                 $("#counter").text(counter + "seconds");
             }
             else {
-                alert("Time is up!");
+                //time is up in card for a few seconds
+
                 clearInterval(clock);
             }
     }}
@@ -104,28 +118,25 @@ $(document).ready(function () {
     function endGame() {
         $("#question").hide();
         $(".button").hide();
-        alert("Time is out!");
-        ("#start").html("Replay")
+        alert("Trivia Game Complete!");
+        ("#start").text("Replay")
         ("#start").show()
     }
 
-    function timesUp() {
-        $("#time").hide();
-        $("#counter").hide();
-        $("#question").hide();
-        $(".button").hide();
-
-    }
+    
     function nextQuestion(){
         displayQuestion();
         displayAnswers();
-        counter=20;
+        $(".card-img-top").attr("src",trivia[index].picture)
+        $(".card-title").hide();
+        counter=20 + "seconds";
         timer();
     }
 
 
 
     //start trivia
+    $(".card").hide();
     $("#time").hide();
     $("#counter").hide();
     $("#question").hide();
@@ -138,26 +149,28 @@ $(document).ready(function () {
     $(".button").on("click", function () {
         userGuess = $(this).attr("id");
         console.log("User Guess: " + userGuess);
-        if (count === trivia.length + 1) {
+        if (counter === 0){
+            //timesUp();
+        }
+        else if (index === trivia.length + 1) {
             endGame();
         }
-        else if (userGuess === trivia[count].answer) {
-            alert("Correct!");
+        else if (userGuess === trivia[index].answer) {
+            $(".card-title").text("CORRECT!")
+            $(".card-title").show();
+            //need to hide question and let the "correct" message linger for a bit
             correctAnswers++;
-            count++;
+            index++;
             nextQuestion();
             
         }
-        else if (userGuess !== trivia[count].answer) {
-            alert("Incorrect! The correct answer was " + trivia[count].answer + ".");
+        else if (userGuess !== trivia[index].answer) {
+            $("#card-title").text("INCORRECT! The correct answer was " + trivia[index].answer + ".");
+            $(".card-title").show();
             incorrectAnswers++;
-            count++;
+            index++;
             nextQuestion();
         }
-        // else if (counter === 0){
-        //     timesUp();
-        // }
-
         
 
 
